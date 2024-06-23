704fc04ee3b486e35757db810d5fe468718c1fb4
atharv <atharvjuwekar.aj@gmail.com>
2024-06-23 14:30:25 +0530

    Initial commit & java inputs code added

---
+++ src/java_programs/JavaInputs.java
@@ -0,0 +1,12 @@
+package java_programs;
+
+import java.util.Scanner;
+
+public class JavaInputs {
+	public static void main(String[] args) {
+		System.out.println("Hello, Enter any value:");
+		Scanner input = new Scanner(System.in);
+		System.out.println("Entered value is : " + input.next());
+	}
+
+}
