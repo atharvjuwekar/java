94ea69a9884679cfec15c6b2866bad723b0eac6d
atharv <atharvjuwekar.aj@gmail.com>
2024-06-23 15:39:05 +0530

    Scanner

--- src/java_programs/JavaInputs.java
+++ src/java_programs/JavaInputs.java
@@ -6,7 +6,9 @@ public class JavaInputs {
 	public static void main(String[] args) {
 		System.out.println("Hello, Enter any value:");
 		Scanner input = new Scanner(System.in);
+		int num = input.nextInt();
 		String sentence = input.nextLine();
+		float marks = input.nextFloat();
 		System.out.println("Entered value is : " + input.nextLine());
 		
 	}
